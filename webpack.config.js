2c650538259428681f9ceb057557c4a6efe9890d
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-05 19:32:21 -0600

    Add webpack and config for react.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+// create a bundle.js, place it in a folder called 'dist'
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  watch: true,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
